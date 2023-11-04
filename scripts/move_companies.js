e2e160bdbcaaf6f9afcf8ab63175abcf3da1e7ca
EgeSaykan <shk.ync2005@gmail.com>
2023-11-04 18:51:26 +0000

    Corrected Planets

--- scripts/move_companies.js
+++ scripts/move_companies.js
@@ -1,15 +1,17 @@
 
-let currentDate = Date.now();
+let exante_time = [Date.now() - Math.random() * 20000];
+let myprotein_time = [Date.now() - Math.random() * 20000];
 
 
-function move_companies(id){
+function move_companies(id, scale, time){
   
-  document.getElementById(id).style.marginTop = ((Date.now() - currentDate) / 25) + "px";
+  document.getElementById(id).style.marginTop = ((Date.now() - time[0]) / scale) + "px";
   a = document.getElementById(id).style.marginTop;
   if (parseInt(a.slice(0, a.length - 2)) >= window.innerHeight) {
-    currentDate = Date.now()
+    time[0] = Date.now()
   }
 
 }
 
-setInterval(move_companies, 5, "exante")
+setInterval(move_companies, 5, "exante", 25, exante_time)
+setInterval(move_companies, 5, "myprotein", 50, myprotein_time)
