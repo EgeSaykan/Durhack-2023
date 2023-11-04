d215f003b3bc96f71758ddd0358f3d1e23954c6d
EgeSaykan <shk.ync2005@gmail.com>
2023-11-04 21:15:29 +0000

    Scoring Added

--- js/game.js
+++ js/game.js
@@ -13,6 +13,29 @@ function updateRocketPosition() {
   rocket.style.left = rocketLeft + 'px';
 }
 
+// Scoring
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+
+  
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
 // Function to update the rocket's velocity and position
 function updateRocket() {
     if (rocketVelocity > -20 & rocketVelocity < 20) {
--- scripts/move_companies.js
+++ scripts/move_companies.js
@@ -2,6 +2,10 @@
 let exante_time = [Date.now() - Math.random() * 20000];
 let myprotein_time = [Date.now() - Math.random() * 20000];
 let selfish = [Date.now() + Math.random() * 20000];
+let glossy = [Date.now() + Math.random() * 20000];
+let espa = [Date.now() + Math.random() * 20000];
+let lookfantastic = [Date.now() + Math.random() * 20000];
+let matalan = [Date.now() + Math.random() * 20000];
 
 
 function move_companies(id, scale, time){
@@ -9,11 +13,15 @@ function move_companies(id, scale, time){
   document.getElementById(id).style.marginTop = ((Date.now() - time[0]) / scale) + "px";
   a = document.getElementById(id).style.marginTop;
   if (parseInt(a.slice(0, a.length - 2)) >= window.innerHeight) {
-    time[0] = Date.now() - Math.random() * 20000
+    time[0] = Date.now() + Math.random() * 20000
   }
 
 }
 
 setInterval(move_companies, 5, "exante", 25, exante_time)
-setInterval(move_companies, 5, "myprotein", 50, myprotein_time)
-setInterval(move_companies, 5, "selfish", 50, selfish)
+setInterval(move_companies, 5, "myprotein", 70, myprotein_time)
+setInterval(move_companies, 5, "selfish", 15, selfish)
+setInterval(move_companies, 5, "glossy-box", 35, glossy)
+setInterval(move_companies, 5, "espa", 45, espa)
+setInterval(move_companies, 5, "lookfantastic", 22, lookfantastic)
+setInterval(move_companies, 5, "matalan", 30, matalan)
