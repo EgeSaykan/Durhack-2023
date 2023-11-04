48b0be63caf924cfe6f597ed4624479f07949861
RobustGuava <147380829+RobustGuava@users.noreply.github.com>
2023-11-04 16:41:36 +0000

    Rocket movement

--- js/game.js
+++ js/game.js
@@ -1,23 +1,43 @@
-const rocket = document.getElementById("rocket");
-        let rocketLeft = 0;
 
-        function updateRocketPosition() {
-            rocket.style.left = rocketLeft + "px";
-        }
+  const rocket = document.getElementById('rocket');
+  const container = document.getElementById('image-container');
+  const rocketWidth = rocket.offsetWidth;
 
-        window.addEventListener("keydown", (event) => {
-            switch (event.key) {
-                case "ArrowLeft":
-                    rocketLeft -= 10;
-                    break;
-                case "ArrowRight":
-                    rocketLeft += 10;
-                    break;
-            }
+  // Set the initial position of the rocket
+  let rocketLeft = (container.offsetWidth - rocketWidth) / 2;
+  let rocketVelocity = 0;
+  const acceleration = 0.5;
+  const friction = 0.02;
 
-            updateRocketPosition();
-        });
+  // Function to update the rocket's position
+  function updateRocketPosition() {
+    rocket.style.left = rocketLeft + 'px';
+  }
 
-        // To center the rocket initially:
-        rocketLeft = (window.innerWidth - rocket.width) / 2;
-        updateRocketPosition();
+  // Function to update the rocket's velocity and position
+  function updateRocket() {
+    rocketLeft += rocketVelocity;
+    rocketVelocity *= 1 - friction;
+    updateRocketPosition();
+    requestAnimationFrame(updateRocket);
+  }
+
+  // Event listener to capture arrow key presses
+  window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        rocketVelocity -= acceleration;
+        break;
+      case 'ArrowRight':
+        rocketVelocity += acceleration;
+        break;
+    }
+  });
+
+  window.addEventListener('keyup', () => {
+    // Apply friction when no arrow keys are pressed
+    rocketVelocity *= 1 - friction;
+  });
+
+  // Start the animation loop
+  updateRocket();
