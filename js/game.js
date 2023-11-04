70addab9e3d10b4d8c4806f8f738349c3d920d3b
RobustGuava <147380829+RobustGuava@users.noreply.github.com>
2023-11-04 20:06:41 +0000

    Improved movement of rocket

--- js/game.js
+++ js/game.js
@@ -1,43 +1,51 @@
+const rocket = document.getElementById('rocket');
+const container = document.getElementById('image-container');
+const rocketWidth = rocket.offsetWidth;
 
-  const rocket = document.getElementById('rocket');
-  const container = document.getElementById('image-container');
-  const rocketWidth = rocket.offsetWidth;
+// Set the initial position of the rocket
+let rocketLeft = (container.offsetWidth - rocketWidth) / 2;
+let rocketVelocity = 0;
+let acceleration = 0;
+const friction = 0.08;
 
-  // Set the initial position of the rocket
-  let rocketLeft = (container.offsetWidth - rocketWidth) / 2;
-  let rocketVelocity = 0;
-  const acceleration = 0.5;
-  const friction = 0.02;
+// Function to update the rocket's position
+function updateRocketPosition() {
+  rocket.style.left = rocketLeft + 'px';
+}
 
-  // Function to update the rocket's position
-  function updateRocketPosition() {
-    rocket.style.left = rocketLeft + 'px';
+// Function to update the rocket's velocity and position
+function updateRocket() {
+    if (rocketVelocity > -20 & rocketVelocity < 20) {
+        rocketVelocity += acceleration
+    };
+  rocketLeft += rocketVelocity;
+  rocketVelocity *= 1 - friction;
+  updateRocketPosition();
+  requestAnimationFrame(updateRocket);
+}
+
+// Event listener to capture arrow key presses
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      acceleration = -2;
+      break;
+    case 'ArrowRight':
+      acceleration = 2;
+      break;
   }
+});
 
-  // Function to update the rocket's velocity and position
-  function updateRocket() {
-    rocketLeft += rocketVelocity;
-    rocketVelocity *= 1 - friction;
-    updateRocketPosition();
-    requestAnimationFrame(updateRocket);
-  }
-
-  // Event listener to capture arrow key presses
-  window.addEventListener('keydown', (event) => {
+window.addEventListener('keyup', (event) => {
     switch (event.key) {
       case 'ArrowLeft':
-        rocketVelocity -= acceleration;
+        acceleration = 0;
         break;
       case 'ArrowRight':
-        rocketVelocity += acceleration;
+        acceleration = 0;
         break;
     }
   });
 
-  window.addEventListener('keyup', () => {
-    // Apply friction when no arrow keys are pressed
-    rocketVelocity *= 1 - friction;
-  });
-
-  // Start the animation loop
-  updateRocket();
+// Start the animation loop
+updateRocket();
