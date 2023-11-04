02ceda3ef51ecbc1bea12f606b5c8abf82c8a18d
William Morgan <Williamdamorgan16@gmail.com>
2023-11-04 16:19:36 +0000

    Rocket now moves

--- js/game.js
+++ js/game.js
@@ -1,23 +1,31 @@
 const rocket = document.getElementById("rocket");
-        let rocketLeft = 0;
+const rocketContainer = document.getElementById("image-container"); // Add this line
 
-        function updateRocketPosition() {
-            rocket.style.left = rocketLeft + "px";
-        }
+console.log("Start");
 
-        window.addEventListener("keydown", (event) => {
-            switch (event.key) {
-                case "ArrowLeft":
-                    rocketLeft -= 10;
-                    break;
-                case "ArrowRight":
-                    rocketLeft += 10;
-                    break;
-            }
+// Set the initial position of the rocket
+let rocketLeft = 0;
 
-            updateRocketPosition();
-        });
+// Function to update the rocket's position
+function updateRocketPosition() {
+  rocket.style.left = rocketLeft + "px";
+}
 
-        // To center the rocket initially:
-        rocketLeft = (window.innerWidth - rocket.width) / 2;
-        updateRocketPosition();
+// Event listener to capture arrow key presses
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      rocketLeft -= 10;
+      break;
+    case "ArrowRight":
+      rocketLeft += 10;
+      break;
+  }
+
+  // Update the rocket's position
+  updateRocketPosition();
+});
+
+// Adjust the rocket's initial position based on its container's width
+rocketLeft = (rocketContainer.offsetWidth - rocket.offsetWidth) / 2;
+updateRocketPosition();
