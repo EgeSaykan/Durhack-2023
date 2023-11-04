8663132d838eabde559314ced941ca96f921ce61
William Morgan <Williamdamorgan16@gmail.com>
2023-11-04 21:28:00 +0000

    Stars now fall plus meteors #8 #4 #2

--- js/starGeneration.js
+++ js/starGeneration.js
@@ -1,45 +1,58 @@
-document.addEventListener("DOMContentLoaded", function() {
-  (function () {
-    function Stars(numberOfStars, divID) {
+(function () {
+  function StarsAndMeteors(numberOfStars, numberOfMeteors, divID) {
       var chosenDiv = document.getElementById(divID);
       chosenDiv.style.display = "none";
       chosenDiv.innerHTML = "";
+
       function randomFrom(array) {
-        return array[Math.floor(Math.random() * array.length)];
+          return array[Math.floor(Math.random() * array.length)];
       }
+
+      var bigRange = Array.apply(null, Array(100)).map(function (_, i) {
+          return i;
+      });
+
       var text = "";
-      for (var i = 0; i < numberOfStars; i++) {
-        var bigRange = Array.apply(null, Array(100)).map(function (_, i) {
-          return i;
-        });
-        var smallRange = Array.apply(null, Array(3)).map(function (_, i) {
-          return i;
-        });
-        var tenRange = Array.apply(null, Array(5)).map(function (_, i) {
-          return i;
-        });
-        var starTwinkleStage = randomFrom(tenRange);
-        var top = randomFrom(bigRange);
-        var right = randomFrom(bigRange);
-        var width = randomFrom(smallRange);
-        text +=
-          "<div class='stars' style='top:" +
-          top +
-          "%; right: " +
-          right +
-          "%; width:" +
-          width +
-          "px; height:" +
-          width +
-          "px;";
-        text += "animation: twinkling " + starTwinkleStage + "s infinite;";
-        text += "box-shadow: 0px 0px 1vw rgba(255, 255, 255, 0.2);'></div>";
+
+      for (var j = 0; j < numberOfMeteors; j++) {
+          var meteorRight = randomFrom(bigRange);
+          var meteorTop = randomFrom(bigRange);
+          text +=
+              "<div class='meteors' style='right:" +
+              meteorRight +
+              "%; top: " +
+              meteorTop +
+              "%;'></div>";
       }
+
+      for (var i = 0; i < numberOfStars; i++) {
+          var smallRange = Array.apply(null, Array(3)).map(function (_, i) {
+              return i;
+          });
+          var tenRange = Array.apply(null, Array(5)).map(function (_, i) {
+              return i;
+          });
+          var starTwinkleStage = randomFrom(tenRange);
+          var top = randomFrom(bigRange);
+          var right = randomFrom(bigRange);
+          var width = randomFrom(smallRange);
+          text +=
+              "<div class='stars' style='top:" +
+              top +
+              "%; right: " +
+              right +
+              "%; width:" +
+              width +
+              "px; height:" +
+              width +
+              "px;";
+          text += "box-shadow: 0px 0px 1vw rgba(255, 255, 255, 0.2);'></div>";
+      }
+
       chosenDiv.innerHTML = text;
       chosenDiv.style.display = "block";
-    }
-  
-    // Call the Stars function to populate the "stars-container" with stars
-    Stars(80, "stars-container");
-  })();
-});
+  }
+
+  // Call the StarsAndMeteors function to populate the "stars-container" with stars and meteors
+  StarsAndMeteors(300, 5, "stars-container"); // Adjust the number of stars and meteors as needed
+})();
